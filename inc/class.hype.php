25497146ffdf10bb9d87d2c1d5f03b3f31683cc8
Drewry Morris <dhmorris@gmail.com>
2013-02-02 17:34:21 -0500

    updating the backend

--- inc/class.hype.php
+++ inc/class.hype.php
@@ -6,5 +6,88 @@ class Hype {
 	public function Hype() {
 		$this->db = Database::obtain();
 	}
+
+	public function get_user($user_id) {
+		$row = $this->db->query_first("SELECT * FROM users WHERE id = '$user_id'");
+
+		// if avatar is empty check gravatar and update
+		if($row['avatar'] == '') {
+			$avatar = $this->get_gravatar($row['email']);
+			$row['avatar'] = $avatar;
+			$this->db->update('users', array('avatar' => $avatar), "id = '$user_id'");
+		}
+
+		return $row;
+	}
+
+	// this is the real sign up process which sends an email and saves a password
+	public function signup($user_id, $email) {
+		$pass = $this->random_password();
+		
+		// updates
+		$this->db->update('users', array('email' => $email), "id = '$user_id'");
+		$this->db->query("SELECT save_Password($user_id, $pass, uuid())");
+
+		// send email
+		$to = $email;
+		$subject = "Welcome to Hypepotamus";
+		$txt = "You're now checked in at the Hype!  Here is your password to login to the website: $pass";
+		$headers = "From: no-reply@hypepotamus.com";
+
+		mail($to,$subject,$txt,$headers);
+
+	}
+
+	// this function just quickly creates a user with avatar name and stuff
+	public function create_user($data) {
+		$data['created'] = 'NOW()';
+		$user = $this->db->insert('users', $data);
+
+		return $user;
+	}
+
+	public function foursquare($raw) {
+		$data = json_decode($raw);
+
+		// check if user exists
+		$row = $this->db->query_first("SELECT id FROM users WHERE id = '$foursquare'");
+		if($row) {
+			$user_id = $row['id'];
+			$checkin = $this->db->query("SELECT check_In( $user_id, 2)");
+		} else {
+			$create_user = array(
+				'firstname' => $data->user->firstName,
+				'lastname' => $data->user->lastName,
+				'avatar' => $data->user->photo
+			);
+			$user_id = $this->create_user($create_user);
+			$checkin = $this->db->query("SELECT check_In( $user_id, 2)");
+		}
+
+		return $checkin;
+	}
 	
+	public function random_password() {
+		$alphabet = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789";
+		for ($i = 0; $i < 8; $i++) {
+			$n = rand(0, count($alphabet)-1);
+			$pass[$i] = $alphabet[$n];
+		}
+
+		return $pass;
+	}
+
+	public function get_gravatar( $email, $s = 80, $d = 'mm', $r = 'g', $img = false, $atts = array() ) {
+		$url = 'http://www.gravatar.com/avatar/';
+		$url .= md5( strtolower( trim( $email ) ) );
+		$url .= "?s=$s&d=$d&r=$r";
+		if ( $img ) {
+			$url = '<img src="' . $url . '"';
+			foreach ( $atts as $key => $val )
+				$url .= ' ' . $key . '="' . $val . '"';
+			$url .= ' />';
+		}
+		return $url;
+	}
+
 } 
--- index.php
+++ index.php
@@ -1,4 +1,51 @@
 <?php
 require_once('inc/config.php');
 
-echo 'Hypepotamus';
+if(!isset($_GET['p'])) {
+	$_GET['p'] = 'home';
+}
+
+switch($_GET['p']) {
+
+	case 'foursquare' :
+		$h->foursquare($_POST['checkin']);
+		break;
+
+	case 'checkin' :
+		if(isset($_POST['checkin'])) {
+			unset($_POST['checkin']);
+			extract($_POST);
+
+			$h->db->query("SELECT check_In($user_id, 1)");
+
+			$user = $h->get_user($user_id);
+
+			echo json_encode($user);
+			exit;
+		}
+		
+		break;
+
+	case 'signup' :
+		if(isset($_POST['signup'])) {
+			unset($_POST['signup']);
+			extract($_POST);
+
+			// add a new user row, sign them up and check them in
+			$user_id = $h->create_user($_POST);
+			$h->signup($user_id, $email);
+			$h->db->query("SELECT check_In($user_id, 1)");
+
+			$user = $h->get_user($user_id);
+
+			echo json_encode($user);
+			exit;
+		}
+
+		break;
+
+	default :
+		echo 'Welcome to the Hype!';
+		break;
+
+}
